262d4bd68aab16b973804613adb2bef587e6e186
BilliMorgan <shimko20@gmail.com>
2020-05-01 13:30:21 +0000

    admmin categories feature added

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, :categories,  except: [:edit, :update, :show]
+    
   end
 
   get "/about" => "about#show"
